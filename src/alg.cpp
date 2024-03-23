b6de6b4687c4adcc98077867c94a73c6bfbdb179
peregomon <160238970+peregomon@users.noreply.github.com>
2024-03-23 16:10:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,11 @@
 // Copyright 2021 NNTU-CS
 
-int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int cbinsearch(int* arr, int size, int value) {
+    int cnt{};
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == value)
+            cnt++;
+    }
+    return cnt;
 }
+
